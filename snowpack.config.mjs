518aa2aa995237d120de175c414c6ee180570d07
ArtemNazarov <art_nazarov@mail.ru>
2021-07-29 17:47:51 +0300

    Migrated to snowpack

---
+++ snowpack.config.mjs
@@ -0,0 +1,33 @@
+/** @type {import("snowpack").SnowpackUserConfig } */
+export default {
+  mount: {
+    public: '/',
+    src: '/dist'
+  },
+  plugins: [
+    /* ... */
+    '@snowpack/plugin-svelte',
+    '@snowpack/plugin-postcss',
+  ],
+  alias: {
+		"@": "./src"
+	},
+  routes: [
+    /* Enable an SPA Fallback in development: */
+    // {"match": "routes", "src": ".*", "dest": "/index.html"},
+  ],
+  optimize: {
+    /* Example: Bundle your final build: */
+    // "bundle": true,
+  },
+  packageOptions: {
+    /* ... */
+  },
+  devOptions: {
+    /* ... */
+  },
+  buildOptions: {
+    out: "dist",
+
+  },
+};
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import App from './App.svelte'
-import './assets/global.css';
+import './global.css';
 
 const app = new App({
   target: document.getElementById('app')
