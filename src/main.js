c6ffebcffcbaca4d750f2216a8028dc9dd2258a1
artem <art_nazarov@mail.ru>
2023-01-29 02:31:37 +0300

    Removed Svelte. Replaced snowpack with vite.js. Added section about privacy.

--- src/main.js
+++ src/main.js
@@ -1,8 +1,27 @@
-import App from './App.svelte'
-import './global.css';
+import anime from "animejs/lib/anime.es.js";
 
-const app = new App({
-  target: document.getElementById('app')
+window.addEventListener('DOMContentLoaded', (event) => {
+    document.getElementById("year").innerHTML = new Date().getFullYear().toString()
+    const textWrapper = document.getElementById("product-slogan");
+    textWrapper.innerHTML = textWrapper.textContent.replace(
+        /./g,
+        "<span class='letter'>$&</span>"
+    );
+
+    anime
+        .timeline({ loop: false })
+        .add({
+            targets: "#product-slogan .letter",
+            opacity: [0, 1],
+            easing: "easeInOutQuad",
+            duration: 2250,
+            delay: (el, i) => 150 * (i + 1),
+        })
+        .add({
+            targets: "#product-slogan h1",
+            opacity: 0,
+            duration: 1000,
+            easing: "easeOutExpo",
+            delay: 1000,
+        });
 })
-
-export default app
--- src/views/NotFoundPage.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-</script>
-
-<div>
-    <h1>404: Page Not Found</h1>
-
-    <a href="/"><button>Return to Home</button></a>
-</div>
